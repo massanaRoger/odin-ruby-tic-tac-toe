b37fd0f624c6350357bd74e9fb5e976eb94b4528
Roger Massana <rogermassana2002@gmail.com>
2022-08-06 22:12:30 +0200

    function play_round prints game to screen

--- tic_tac.rb
+++ tic_tac.rb
@@ -1,10 +1,23 @@
 # frozen_string_literal: true
 
 class Game
-  @end = false
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
+    @end = false
+    @game_array = Array.new(9, '%')
+  end
+
+  def play_round
+    print_screen
+  end
+
+  private
+
+  def print_screen
+    puts "|#{@game_array[0]}|#{@game_array[1]}|#{@game_array[2]}|"
+    puts "|#{@game_array[3]}|#{@game_array[4]}|#{@game_array[5]}|"
+    puts "|#{@game_array[6]}|#{@game_array[7]}|#{@game_array[8]}|"
   end
 end
 
@@ -13,3 +26,8 @@ class Player
     @name = name
   end
 end
+
+player1 = Player.new('Roger')
+player2 = Player.new('XD')
+game = Game.new(player1, player2)
+game.play_round
